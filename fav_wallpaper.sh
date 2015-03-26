4feb2f440d268dfd62b750263bff7ac6b3c30cf7
Jeffrey Cutter <jcutter@redhat.com>
2015-03-26 01:08:30 -0400

    Added an echo and a sleep so you can see which file it's adding.

--- fav_wallpaper.sh
+++ fav_wallpaper.sh
@@ -3,6 +3,8 @@
 FAVORITES_FILE=~/var/wallpaper.favorites
 CURRENT_FILE=~/var/wallpaper.current
 
+echo "Saving $CURRENT_FILE to $FAVORITES_FILE."
+
 touch $FAVORITES_FILE
 
 FAVORITES=$(cat $FAVORITES_FILE $CURRENT_FILE | sort -u)
@@ -13,3 +15,5 @@ for i in $FAVORITES
 do
   echo $i >> $FAVORITES_FILE
 done
+
+sleep 2
